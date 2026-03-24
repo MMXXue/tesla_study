b8a8e08c58b3a48d6805ddfad8a5fc28a85fad98
Sheldon Ma <maxueyuan1209@163.com>
2026-03-25 00:16:36 +1100

    chore: 双数之和(指针解法)

--- backend/leetcode/两数之和.py
+++ backend/leetcode/两数之和.py
@@ -5,19 +5,34 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        m = 0
-        result = []
-        print(len(nums))
+        # 暴力
+        # m = 0
+        # result = []
+        # print(len(nums))
 
-        for m in range(len(nums) - 1):
-            print(f"m: {m}")
-            for n in range(m+1, len(nums)):
-                print(f"n: {n}")
-                if nums[m] + nums[n] == target:
-                    result.append(m)
-                    result.append(n)
-                    return result
-            print(f"\n")
+        # for m in range(len(nums) - 1):
+        #     print(f"m: {m}")
+        #     for n in range(m+1, len(nums)):
+        #         print(f"n: {n}")
+        #         if nums[m] + nums[n] == target:
+        #             result.append(m)
+        #             result.append(n)
+        #             return result
+        #     print(f"\n")
+
+
+        # 指针
+        prev_map = {}
+
+        # enumerate总是会返回两个数值,第一个是索引,第二个是数值
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prev_map:
+                return [prev_map[n], i]
+            prev_map[n] = i
+
+
+
 
 
 
